d195f315fd2824e87bf8bc8744623c109ef24e2a
MrDotGamer <40805382+MrDotGamer@users.noreply.github.com>
2020-06-09 00:13:49 +0300

    Atsakymas

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace UVS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.thread_count_nud = new System.Windows.Forms.NumericUpDown();
+            this.start_btn = new System.Windows.Forms.Button();
+            this.thread_lv = new System.Windows.Forms.ListView();
+            this.Data = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ThreadId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.stop_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.thread_count_nud)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // thread_count_nud
+            // 
+            this.thread_count_nud.Location = new System.Drawing.Point(12, 12);
+            this.thread_count_nud.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.thread_count_nud.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.thread_count_nud.Name = "thread_count_nud";
+            this.thread_count_nud.Size = new System.Drawing.Size(41, 20);
+            this.thread_count_nud.TabIndex = 3;
+            this.thread_count_nud.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // start_btn
+            // 
+            this.start_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.start_btn.Location = new System.Drawing.Point(75, 387);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.start_btn.Size = new System.Drawing.Size(149, 27);
+            this.start_btn.TabIndex = 4;
+            this.start_btn.Text = "Start";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // thread_lv
+            // 
+            this.thread_lv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.thread_lv.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Data,
+            this.ThreadId});
+            this.thread_lv.FullRowSelect = true;
+            this.thread_lv.GridLines = true;
+            this.thread_lv.HideSelection = false;
+            this.thread_lv.Location = new System.Drawing.Point(75, 12);
+            this.thread_lv.MultiSelect = false;
+            this.thread_lv.Name = "thread_lv";
+            this.thread_lv.Size = new System.Drawing.Size(322, 369);
+            this.thread_lv.TabIndex = 5;
+            this.thread_lv.UseCompatibleStateImageBehavior = false;
+            this.thread_lv.View = System.Windows.Forms.View.Details;
+            // 
+            // Data
+            // 
+            this.Data.DisplayIndex = 1;
+            this.Data.Text = "Generuojama eilute";
+            this.Data.Width = 100;
+            // 
+            // ThreadId
+            // 
+            this.ThreadId.DisplayIndex = 0;
+            this.ThreadId.Text = "ThreadId";
+            this.ThreadId.Width = 109;
+            // 
+            // stop_btn
+            // 
+            this.stop_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.stop_btn.Location = new System.Drawing.Point(248, 387);
+            this.stop_btn.Name = "stop_btn";
+            this.stop_btn.Size = new System.Drawing.Size(149, 27);
+            this.stop_btn.TabIndex = 6;
+            this.stop_btn.Text = "Stop";
+            this.stop_btn.UseVisualStyleBackColor = true;
+            this.stop_btn.Click += new System.EventHandler(this.stop_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 420);
+            this.Controls.Add(this.stop_btn);
+            this.Controls.Add(this.thread_lv);
+            this.Controls.Add(this.start_btn);
+            this.Controls.Add(this.thread_count_nud);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.thread_count_nud)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown thread_count_nud;
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.ListView thread_lv;
+        private System.Windows.Forms.Button stop_btn;
+        private System.Windows.Forms.ColumnHeader Data;
+        private System.Windows.Forms.ColumnHeader ThreadId;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+//1)  Formoje vartotojas gali nurodyti kintamą kiekį thread(atšakos) (Nuo 2 iki 15, kiekis negali būti didesnis ar mažesnis).
+//2)  Išrinkus kiekį ir paspaudus start mygtuką, kiekvienas thread turi atsitiktiniu laiko intervalu(0,5-2 sekundžių) generuoti 5-10 (atsitiktinai) simbolių ilgio eilutę.
+//3)  Turi būti įsimenamos/rodomos 20 paskutinių sugeneruotų duomenų, kurie išvedami į formos ListView kontrolą.ListView turi tokias kolonas – Thread ID(numeris atšakos, kur numeruojama nuo 1), sugeneruota eilutė.
+//4)  Visi atšakos generuoti duomenis rašomi į access duomenų failą(mdb) (galite naudoti ir SQL serverį, jie turite tokią galimybę), lentelę - kur laukai yra ID(autonumber), ThreadID, Time(laikas sugeneravimo), Data(eilutė).
+//5)  Paspaudus Stop mygtuką darbas stabdomas ir thread išjungiamos.
+namespace UVS
+{
+    public partial class Form1 : Form
+    {
+        const string Connection = "Server=(local)\\sqlexpress;Database=ThreadData;Trusted_Connection=True;MultipleActiveResultSets = true";
+        const string Symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        public bool CanWork { get; set; }
+        Thread[] threadsArray = null;
+        int total_thread_num;
+        public delegate void PopulateListview();
+        PopulateListview populateView;
+
+        public Form1()
+        {
+            populateView = new PopulateListview(PopulateListView);
+            CanWork = true;
+            InitializeComponent();
+            populateView();
+        }
+
+        private void PopulateListView()
+        {
+            SqlConnection con = new SqlConnection(Connection);
+            SqlCommand query = new SqlCommand("SELECT top 20 ThreadId,Data FROM ThreadII ORDER BY ID DESC", con);
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new MethodInvoker(delegate {
+                    PopulateListView();
+                }));
+                return;
+            }
+            if(thread_lv != null)
+            {
+                thread_lv.Items.Clear();
+            }
+            try
+            {
+                con.Open();
+                SqlDataReader sdr = query.ExecuteReader();
+                while (sdr.Read())
+                {
+                    ListViewItem item = new ListViewItem(sdr["Data"].ToString());
+                    item.SubItems.Add(sdr["ThreadId"].ToString());
+                    thread_lv.Items.Add(item);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error :{0}", e);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            CanWork = true;
+            ThreadGenerator();
+        }
+
+        private void stop_btn_Click(object sender, EventArgs e)
+        {
+            CanWork = false;
+        }
+
+        private void ThreadGenerator()
+        {
+            total_thread_num = Convert.ToInt32(Math.Round(thread_count_nud.Value, 0));
+            threadsArray = null;
+            threadsArray = new Thread[total_thread_num];
+            for (int i = 0; i < total_thread_num; i++)
+            {
+                threadsArray[i] = new Thread(()=>Generator());
+                threadsArray[i].Name = (i+1).ToString();
+                threadsArray[i].Start();
+                Thread.Sleep(100);
+                Console.WriteLine("Thread nr {0} start",i);
+            }
+        }
+
+        private void Generator()
+        {
+            try
+            {
+                while (CanWork)
+                {
+                    Thread.Sleep(Random_Interval(500, 2000));
+                    DataGenerator();
+                }
+            }
+            catch (ThreadInterruptedException)
+            {
+                Thread.CurrentThread.Interrupt();
+            }
+            finally
+            {
+                    Thread.CurrentThread.Interrupt();
+                    Console.WriteLine("Thread sekmingai sustabdytas");
+            }
+        }
+
+        public void DataGenerator()
+        {
+            int thread_nr = int.Parse(Thread.CurrentThread.Name);
+            DateTime dateTimeNow = DateTime.Now;
+            Random random = new Random();
+            int size = Random_Interval(5, 10);
+            StringBuilder builder = new StringBuilder();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Symbols[random.Next(Symbols.Length)];
+                builder.Append(ch);
+            }
+            DataWriter(thread_nr, dateTimeNow, builder.ToString());  
+            Console.WriteLine("thread nr {0} data: {1} timestamp {2}", thread_nr, builder.ToString(), dateTimeNow);
+        }
+
+        private void DataWriter(int thread_nr, DateTime dateTime, string thread_data)
+        {
+            SqlConnection con;
+            SqlCommand com;
+            con = new SqlConnection(Connection);
+            con.Open();
+            com = new SqlCommand("insert into ThreadII(Data,ThreadId,Date) values('" + thread_data + "'," + thread_nr + ",'" + dateTime + "')", con);
+            try
+            {
+                com.ExecuteNonQuery();
+                Console.WriteLine("Saved...");
+                populateView();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Not Saved (Exception {0}):", e);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private static int Random_Interval(int min, int max)
+        {
+            Random interval = new Random();
+            return interval.Next(min, max);
+        }
+
+        private void CreateDataTable()
+        {
+            SqlConnection con = new SqlConnection(Connection);
+            SqlCommand cmd = new SqlCommand(@"CREATE TABLE dbo.ThreadII
+                (
+                    ID int IDENTITY(1, 1) NOT NULL,
+                    Data nvarchar(10) NULL,
+                    ThreadId int  NULL,
+                    Date datetime NULL,
+                    CONSTRAINT pk_id PRIMARY KEY(ID)
+                )", con);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Table Created Successfully");
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error Generated. Details: " + e.ToString());
+            }
+            finally
+            {
+                con.Close();
+                Console.ReadKey();
+            }
+        }
+    }
+}
